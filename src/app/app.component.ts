73b707dd6093a74c33b9d556d9d612d0fb6467f7
Kyle Hall <hallkd47@gmail.com>
2018-01-27 14:55:41 -0600

    adds sfc changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+  `,
+  styles: [`
+    * {
+      box-sizing: border-box;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'Tour of Heroes';
+
 }
