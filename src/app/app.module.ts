f0359c36935e15d6da7f3e9ab77b69d7c56af447
Kyle Hall <hallkd47@gmail.com>
2018-02-04 06:36:51 -0600

    adds message service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
+import { HeroService } from './services/heroes/hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './services/messages/message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [
-    HeroService
+    HeroService,
+    MessageService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../symbols/hero';
 import { HEROES } from '../mock-heroes';
-import { HeroService } from '../hero.service';
+import { HeroService } from '../services/heroes/hero.service';
 
 @Component({
   selector: 'app-heroes',
